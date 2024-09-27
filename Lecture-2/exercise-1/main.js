3557195ba9aa784277cce39d14e0d6c9cd3b8b1f
Avtandil Naraishvili <101639573+AvtoTheThird@users.noreply.github.com>
2024-09-27 05:00:50 -0700

    Update main.js

--- Lecture-2/exercise-1/main.js
+++ Lecture-2/exercise-1/main.js
@@ -13,32 +13,3 @@ Array.prototype.myFilter = function (callback, context) {
 
   return filteredArray;
 };
-
-// Test case for myFilter
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// Test 1: Filter even numbers
-const evenNumbers = numbers.myFilter(function (num) {
-  return num % 2 === 0;
-});
-console.log("Even numbers:", evenNumbers);
-
-// Test 2: Filter numbers greater than 5
-const greaterThanFive = numbers.myFilter(function (num) {
-  return num > 5;
-});
-console.log("Numbers greater than 5:", greaterThanFive);
-
-// Test 3: Test with context
-const obj = { threshold: 7 };
-const greaterThanThreshold = numbers.myFilter(function (num) {
-  return num > this.threshold;
-}, obj);
-console.log("Numbers greater than threshold:", greaterThanThreshold);
-
-// Test 4: Test with non-function callback (should throw TypeError)
-try {
-  numbers.myFilter("not a function");
-} catch (error) {
-  console.log("Error caught:", error.message);
-}
